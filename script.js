24d73d6dfd626032ce759dec7031e95d4c68e90a
Nogatz <leonardonogatz@gmail.com>
2015-04-24 21:32:37 -0300

    decima

--- script.js
+++ script.js
@@ -4,24 +4,24 @@
 	var db;
 	function onDeviceReady(){
 			console.log("ondevice ready");
-			db = window.openDatabase("medidor", "1.0", "medidor", 2*1024*1024);
-			db.transaction(createDb, erroDB, successDB);
+			db = window.openDatabase("Medidor", "1.0", "medidorbd", 2*1024*1024);
+			db.transaction(createDb, erroCB, successCB);
 		}
 
 	function createDb(tx){
 		console.log("create");
-			tx.executeSql('DROP TABLE IF EXISTS BEBIDAS');
+			//tx.executeSql('DROP TABLE IF EXISTS BEBIDAS');
 			tx.executeSql('CREATE TABLE IF NOT EXISTS BEBIDAS(nome,qtdalcool,litragem)');
 		}
 
-	function erroDB(err){
-		console.log("error db" +  err.code);
+	function erroCB(err){
+			console.log("error db" +  err.code);
 			alert('Problema com o banco de dados' + err.code );
 		}
 
-	function successDB(){
-		console.log("sucesso db");
-			alert('Tudo certo, conectou' );
+	function successCB(){
+			console.log("sucesso db");
+			//alert('Tudo certo, conectou' );
 		}
 
 	function insertDB(tx){
@@ -29,13 +29,15 @@
 		var _qtdalcool = $("[name='qdt-alcool']").val();			
 		var _litragem = $("[name='litragem-bebida']").val();		
 		var sql = 'INSERT INTO BEBIDAS(nome,qtdalcool,litragem) VALUES(?,?,?)';
-		tx.executeSql(sql, [_nome,_qtdalcool,_litragem], sucessQueryDB, erroDB);
+		tx.executeSql(sql, [_nome,_qtdalcool,_litragem], sucessQueryDB, erroCB);
 		}
 		
 		var htmlstring="";
-	function renderList(){
+	function renderList(tx, results){
 			var htmlstring = '';
-			var len = results.rows.length;		
+			
+			var len = results.rows.length;
+					
 			for (var i=0; i<len; i++){
 				htmlstring += '<li>' + results.rows.item(i).nome + '<li>';
 				}
@@ -45,14 +47,13 @@
 	$('#ListView').listview('refresh');	
 		
 	function sucessQueryDB(){
-		alert("resultado da query");
-		tx.executeSql('SELECT * FROM BEBIDAS', [], renderList, erroDB);
+		tx.executeSql('SELECT * FROM BEBIDAS', [], renderList, erroCB);
 				
 		}
 
 	function submitform(){
 		console.log("teste2");
-		db.transaction(insertDB, erroDB);
+		db.transaction(insertDB, erroCB);
 				console.log("teste3");
 		$.mobile.changePage("#confirmacaocadastro", {reverse: false, transition:"slide"});
 		return false;
